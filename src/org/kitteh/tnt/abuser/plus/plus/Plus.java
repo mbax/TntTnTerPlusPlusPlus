a8af33b748c6f5a60da864ae57a0990e62a879b9
mbax <matt@phozop.net>
2012-12-15 17:26:14 -0500

    Improved to match both parts of username

--- src/org/kitteh/tnt/abuser/plus/plus/Plus.java
+++ src/org/kitteh/tnt/abuser/plus/plus/Plus.java
@@ -1,10 +1,12 @@
 package org.kitteh.tnt.abuser.plus.plus;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Plus extends JavaPlugin implements Listener {
@@ -27,6 +29,8 @@ public class Plus extends JavaPlugin implements Listener {
         if (event.getPlayer().getName().equalsIgnoreCase("tntbass")) {
             if (tntcount == 0) {
                 event.getPlayer().getWorld().createExplosion(event.getPlayer().getLocation(), 2f, true);
+            } else if (tntcount == 5) {
+                event.getPlayer().getWorld().dropItemNaturally(event.getPlayer().getLocation(), new ItemStack(Material.RAW_FISH, 1));
             }
             tntcount++;
             if (tntcount > 10) {
